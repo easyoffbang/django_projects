fde7e13bd5989ef0595ace31fc2def08b37c3576
easyoffbang <bangvi_0819@naver.com>
2019-01-22 05:39:29 +0900

    2019.01.22 최종수정

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,8 @@ from django.urls import path, include, re_path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list')
+    path('', views.post_list, name='post_list'),
+ #   path('^new/$', views.post_new, name='post_new'),
+  #  path('^<\d+:id>/edit/$', views.post_detail, name='post_detail')   #int처럼 속성이 아닌 \d+가 올 수 있는가?
 ]
+
