2e35d1014c0e18dfe42e9bb80c37f36e666c5236
easyoffbang <bangvi_0819@naver.com>
2019-01-23 06:09:49 +0900

    404응답 추가 -ep10

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('<int:id>/', views.post_detail),
  #   path('^new/$', views.post_new, name='post_new'),
   #  path('^<\d+:id>/edit/$', views.post_detail, name='post_detail')   #int처럼 속성이 아닌 \d+가 올 수 있는가?
 ]
