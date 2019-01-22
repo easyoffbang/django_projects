ac0e6850adafffce44a80bd05cd93ec4613be647
easyoffbang <bangvi_0819@naver.com>
2019-01-22 23:42:37 +0900

    blog.models.Post 모델 생성/migration/admin등록

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+class Post(models.Model):
+    title = models.CharField(max_length=100, verbose_name='제목',                      #길이제한이 있는 문자열 -  값필요
+        help_text='포스팅 제목을 입력해주세요. 최대 100자 내외.',
+        choices = (
+            ('제목1', '제목1 레이블'),   #('저장될 값', 'UI에 보여질 레이블')
+            ('제목2', '제목2 레이블'),
+            ('제목3', '제목3 레이블'),
+        ))
+    content = models.TextField(verbose_name='내용')                                  #길이제한이 없는 문자열 -  값필요
+    created_at = models.DateTimeField(auto_now_add=True)          #자동으로 부여
+    updated_at = models.DateTimeField(auto_now=True)              #자동으로 부여
+
 # Create your models here.
