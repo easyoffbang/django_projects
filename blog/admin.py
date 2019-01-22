a9958a22b4a5e0296f2db775a9306672c73a1966
easyoffbang <bangvi_0819@naver.com>
2019-01-23 03:18:25 +0900

    blog.models.Post 모델에 status필드를 추가하고, 이를 처리하는 Custom admin action 구현

--- blog/admin.py
+++ blog/admin.py
@@ -4,10 +4,27 @@ from django.utils.safestring import mark_safe
 # Register your models here.
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'content_size', 'created_at', 'updated_at']
+    list_display = ['id', 'title', 'content_size', 'status', 'created_at', 'updated_at']
+
+    actions = ['make_published']
 
     def content_size(self, post):
         return mark_safe('<strong>{}글자</strong>'.format(len(post.content)))
     content_size.short_description = '글자수'
 
-admin.site.register(Post, PostAdmin)
+    def make_draft(self, request, queryset):
+        updated_count = queryset.update(status='p')
+        self.message_user(request, '{}건의 포스팅을 draft상태로 변경'.format(updated_count))
+    make_draft.short_description = '지정포스팅을 draft상태로 변경합니다.'
+
+    def make_published(self, request, queryset):
+        updated_count = queryset.update(status='p')
+        self.message_user(request, '{}건의 포스팅을 Published상태로 변경'.format(updated_count))
+    make_published.short_description = '지정포스팅을 Published상태로 변경합니다.'
+
+    def make_withdrawn(self, request, queryset):
+        updated_count = queryset.update(status='p')
+        self.message_user(request, '{}건의 포스팅을 withdrawn상태로 변경'.format(updated_count))
+    make_withdrawn.short_description = '지정포스팅을 withdrawn상태로 변경합니다.'
+
+#admin.site.register(Post, PostAdmin)
