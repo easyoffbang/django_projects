cfdd9925d61c5f424668a823f53e02cc1c2b76e9
easyoffbang <bangvi_0819@naver.com>
2019-01-23 01:24:49 +0900

    author필드 추가

--- blog/models.py
+++ blog/models.py
@@ -10,13 +10,14 @@ def lnglat_validator(value):
 
 
 class Post(models.Model):
+    author = models.CharField(max_length=20)
     title = models.CharField(max_length=100, verbose_name='제목',                      #길이제한이 있는 문자열 -  값필요
         help_text='포스팅 제목을 입력해주세요. 최대 100자 내외.',
-        choices = (
-            ('제목1', '제목1 레이블'),   #('저장될 값', 'UI에 보여질 레이블')
-            ('제목2', '제목2 레이블'),
-            ('제목3', '제목3 레이블'),
-        ))
+        #choices = (
+            #('제목1', '제목1 레이블'),   #('저장될 값', 'UI에 보여질 레이블')
+            #('제목2', '제목2 레이블'),
+            #('제목3', '제목3 레이블'),
+        )
     content = models.TextField(verbose_name='내용')                                  #길이제한이 없는 문자열 -  값필요
     tags = models.CharField(max_length=100, blank=True)
     lnglat = models.CharField(max_length=50, blank=True,
