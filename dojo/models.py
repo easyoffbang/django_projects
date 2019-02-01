6cf667cbf213b525731a1c666d3d728af0fcdafe
easyoffbang <bangvi_0819@naver.com>
2019-02-01 15:05:35 +0900

    dojo.forms.PostForm 클래스 선언

--- dojo/models.py
+++ dojo/models.py
@@ -4,5 +4,6 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
+    # db에 저장되는 파일의 타입 TEXTFIELD
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
