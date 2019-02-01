255894f0a03272219a7eb43163e2f9d51b539402
easyoffbang <bangvi_0819@naver.com>
2019-02-01 17:12:49 +0900

    blog.views.post_new/post_edit 뷰를 구현

--- blog/urls.py
+++ blog/urls.py
@@ -10,5 +10,8 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('<int:id>/', views.post_detail, name = 'post_detail'),
-    path('cbv/new/', views_cbv.post_new)
+    path('cbv/new/', views_cbv.post_new),
+
+    path('new/', views.post_new, name='post_new'),
+    re_path(r'^(?P<id>\d+)/edit/$', views.post_edit, name='post_edit'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
 from .models import Post
 from django.http import Http404
+from .forms import PostForm
 # Create your views here.
 
 def post_list(request):
@@ -26,3 +27,28 @@ def post_detail(request, id):
         'post': post,
     })
 
+
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save()
+            return redirect(post)    #post.get_absolute_url()  => post detail
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_form.html', {
+        'form': form,
+        })
+
+def post_edit(request, id):
+    post = get_object_or_404(Post, id=id)
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            post = form.save()
+            return redirect(post)    #post.get_absolute_url()  => post detail
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_form.html', {
+        'form': form,
+        })
