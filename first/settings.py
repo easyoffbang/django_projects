21d85a04cc0fc45ae7b8ec852683001bf5928312
easyoffbang <bangvi_0819@naver.com>
2019-02-10 17:40:03 +0900

    settings.STATIC_ROOT 설정을 추가하고, collectstatic 명령실습

--- first/settings.py
+++ first/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap3',
     'blog',
     'dojo',
     'accounts',
@@ -127,5 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'first', 'static'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 INTERNAL_IPS = ['127.0.0.1']
